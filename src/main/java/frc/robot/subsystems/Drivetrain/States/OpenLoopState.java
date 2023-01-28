2ae1c2cce2d5e921be2ffd5815f1962c320863ed
haydenosborne1213 <hdneosborne@gmail.com>
2023-01-27 18:45:08 -0700

    add OpenLoopState

---
+++ src/main/java/frc/robot/subsystems/Drivetrain/States/OpenLoopState.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.Drivetrain.States;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants.ControllerMap;
+import frc.robot.subsystems.Drivetrain.DrivetrainSubsystem;
+
+public class OpenLoopState extends CommandBase {
+    
+    DrivetrainSubsystem drive = DrivetrainSubsystem.getInstance();
+
+    public OpenLoopState() {
+        addRequirements(drive);
+    }
+
+    @Override
+  public void execute() {
+    double throttle = ControllerMap.getDriveStick().getRawAxis(ControllerMap.GenericHID.Axis.STICK_Y);
+    double rotation = ControllerMap.getDriveStick().getRawAxis(ControllerMap.GenericHID.Axis.STICK_X);
+
+    DrivetrainSubsystem.getInstance().setArcade(throttle, rotation);
+  }
+
+
+
+}
